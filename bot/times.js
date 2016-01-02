287540de230e1cace739dd7df3bcc7fabb31fe6a
TD <TautvydasUK@gmail.com>
2016-01-02 01:35:47 +0000

    make it better

--- bot/times.js
+++ bot/times.js
@@ -1,38 +1,57 @@
 var https = require('https');
 
 module.exports = function() {
-/*
+
+  function postMsg(message, channel) {
+    var req = https.request({
+        hostname: 'hooks.slack.com' ,
+        path: '/services/T0GM8NHU0/B0H9881K8/TE8PaC19TxajzwIhDSun1ge7',
+        method: 'POST'
+    }, function (res, b, c) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          console.log('Data received: ', chunk);
+        });
+    });
+
+    req.on('error', function (e) {
+        console.log('Error while posting message: ' + e.message);
+    });
+
+    req.write(JSON.stringify({
+        "channel": channel || "#tests",
+        "username": "Bender",
+        "text": message
+    }));
+    req.end();
+  };
+
   setInterval(function() {
     var d = new Date(),
       minutes = d.getMinutes(),
-      hours = d.getHours();
+      hours = d.getHours(),
+      days = d.getDays();
 
-    if (hours === 0 && minutes === 0) {
-      var postMessage = 'Yeeha - it\'s Midnight!';
+    if (days < 6) { // No posting on Weekends
 
-      var req = https.request({
-          hostname: 'hooks.slack.com' ,
-          path: '/services/T0GM8NHU0/B0H9881K8/TE8PaC19TxajzwIhDSun1ge7',
-          method: 'POST'
-      }, function (res, b, c) {
-          res.setEncoding('utf8');
-          res.on('data', function (chunk) {
-            console.log('Data received: ', chunk);
-          });
-      });
+      // Morning stand ups announce
+      if (hours === 8 && minutes === 55) {
+        postMsg('*Team 1* it\'s almost stand up time, have a good one!');
+      }
+      else if (hours === 9 && minutes === 15) {
+        postMsg('*Team 2* it\s your turn for a stand up!');
+      }
 
-      req.on('error', function (e) {
-          console.log('problem with request: ' + e.message);
-      });
+      // Lunch time
+      else if (hours === 12 && minutes === 0) {
+        postMsg('Lunch time! Enjoy it :-)');
+      }
 
-      req.write(JSON.stringify({
-          "channel": "#area45" ,
-          "username": "Bender" ,
-          "text": postMessage
-      }));
-      req.end();
+      //
+      else if (hours === 5 && minutes === 5) {
+        postMsg('Before starting your travels home have a check on current tube status. <Command: *!tfl*>');
+      }
     }
 
   }, 60000);
-*/
 };
