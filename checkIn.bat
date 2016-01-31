6dd0128869cfe8225564f86418d9cccf4483a573
TD <TautvydasUK@gmail.com>
2016-01-31 11:12:27 +0000

    Minor fixes

--- checkIn.bat
+++ checkIn.bat
@@ -1,32 +1,21 @@
 @echo off
 
-echo "Param 1: " %1
-echo "Param 2: " %2
-
-REM ------------------------------------------------------------------
-REM SET BRANCH
-
-REM set default branch
-set branch=master
-
-REM if there is a second parameter (branch), we set default branch to the new one (from 2nd parameter)
-IF [%2]==[] (
-echo "no branch given, will commit to default %branch%"
-) ELSE (
-echo "ok, setting branch to %2 ..."
-set %branch%=%2
-)
+echo Commit message: " %1
 
 REM ------------------------------------------------------------------
 REM set commit message
 
+set defaultmessage="Minor fixes"
+
 IF [%1]==[] (
 REM if there is no message, exit
-echo "there is no commit message! exit..."
-Exit /b
+echo "no commit message given, will use default one: %defaultmessage%"
 ) ELSE (
 REM there is a message, lets do a commit
+
+set %defaultmessage%=%1
+
 git add .
-git commit -m %1
-git push origin %branch%
+git commit -am %defaultmessage%
+git push heroku master
 )
