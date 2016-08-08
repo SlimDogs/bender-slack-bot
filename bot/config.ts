2256d70b84d107dda86717879332648c149d275c
TD <TautvydasUK@gmail.com>
2016-08-08 21:07:28 +0100

    Initial github check in

---
+++ bot/config.ts
@@ -0,0 +1,14 @@
+/// <reference path="../typings/index.d.ts" />
+/// <reference path="../interfaces.ts" />
+
+let config: Iconfig = {
+    // Weather API key
+    OPEN_WEATHER_API_TOKEN: '*****',
+    // Jokes Mongo db thingies
+    DB_NAME: '*****',
+    DB_USERNAME: '*****',
+    DB_USER_PASSWORD: '****',
+    DB_URL_ADDRESS: '*****',
+};
+
+module.exports = config;
