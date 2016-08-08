a9b0c21df1905a854bc1174b4863a2c8c9743a72
TD <TautvydasUK@gmail.com>
2016-08-08 21:06:04 +0100

    Initial github check in

--- bot/config.js
+++ bot/config.js
@@ -2,11 +2,11 @@
 /// <reference path="../interfaces.ts" />
 var config = {
     // Weather API key
-    OPEN_WEATHER_API_TOKEN: '89b5367178e25ae61a711df8069000ff',
+    OPEN_WEATHER_API_TOKEN: '*****',
     // Jokes Mongo db thingies
-    DB_NAME: 'heroku_mzb1clx9',
-    DB_USERNAME: 'getJoke',
-    DB_USER_PASSWORD: 'Testas123',
-    DB_URL_ADDRESS: 'ds037005.mongolab.com:37005'
+    DB_NAME: '*****',
+    DB_USERNAME: '*****',
+    DB_USER_PASSWORD: '****',
+    DB_URL_ADDRESS: '*****'
 };
 module.exports = config;
